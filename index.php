a8b3bd9ac25852db03d3e2feb3cc35c16775f5d8
Henrique Lopes Velozo <henriquelopesvelozo@gmail.com>
2022-09-19 22:37:06 -0300

    csgo

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="bg-dark">
+        <div class="row">
+            <div class="container">
+                <div class="row">
+                    <nav class="col navbar navbar-light navbar-expand-md">
+                        <a href="index.html" class="navbar-brand">
+                            <img src="img/gamesacademy.jpg" alt="" class="img-logo">
+                        </a>
+                        <button class="navbar-toggler" data-toggle="collapse" data-target="#principal">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div id="principal" class="collapse navbar-collapse">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item ">
+                                    <a href="" class="nav-link text-light ">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link text-light">Cursos</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link text-light">Benefícios</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link text-light">Contato</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="" class="btn btn-outline-light">Entrar</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+                <header class="row">
+                    <div class="col text-light d-flex">
+                        <div class="align-self-center">
+                            <h1 class="display-4">
+                                Treinamento CSGO:Rumo ao topo!!
+                            </h1>
+                            <p>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col d-none d-md-block text-right">
+                        <img src="img/cold.gif" class="img-fluid" alt="">
+                    </div>
+                </header>
+                <br>
+                <header class="row">
+                    <div class="col text-light d-flex">
+                        <div class="align-self-center">
+                            <br>
+                            <h1 class="display-4">
+                                Cadastre-se
+                            </h1>
+                            <div class="container">
+                                <form action="" class="mt-4 mb-4">
+                                    <label for="InputEmail">Email:</label>
+                                    <input type="text" id="InputEmail" required maxlength="50">
+                                    <span></span>
+                                    <br>
+                                    <label for="InputSenha">Senha:</label>
+                                    <input type="password" id="InputSenha" required maxlength="10">
+                                    <span></span>
+                                    <br>
+                                    <label for="InputPrimeiroNome">Primeiro Nome:</label>
+                                    <input type="text" id="InputPrimeiroNome" required maxlength="8">
+                                    <span></span>
+                                    <br>
+                                    <label for="InputSegundoNome">Segundo Nome:</label>
+                                    <input type="text" id="InputSegundoNome" required maxlength="12">
+                                    <span></span>
+                                    <br>
+                                    <label for="InputCidade-Estado">Cidade-Estado:</label>
+                                    <input type="text" id="InputCidade-Estado" required maxlength="10">
+                                    <span></span>
+                                    <br>
+                                    <button class="btn btn-primary">Enviar</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col d-none d-md-block text-right">
+                        <img src="img/BrazilCSGO.jpg" class="img-fluid" alt="">
+                    </div>
+                </header>
+
+                <div class="container">
+                    <section class="row mt-4 mb-4">
+                        <div class="col-md-6 d-flex">
+                            <div class="align-self-center text-light">
+                                <h2>
+                                    Cansado de ser Chamado de noob? Seus amigos não querem jogar com você?
+                                </h2>
+                                <p>
+                                    Venha para a Gamers Academy. Aqui você ira aprender com os melhores.
+                                </p>
+                                <a href="" class="btn btn-warning">Veja mais</a>
+                            </div>
+                            <hr>
+                        </div>
+                        <div class="col-md-6">
+                            <img src="img/pin.png" class="img-fluid" alt="">
+                        </div>
+                    </section>
+                    <section class="row mt-4 mb-4">
+                        <div class="col-md-6">
+                            <img src="img/simple.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="col-md-6 d-flex">
+                            <div class="align-self-center text-light">
+                                <h2>Independentemente de Sua Patente, Você Não Vai Precisar ter um PC Gamer, ou
+                                    periféricos de grande marca para
+                                    se tornar um dos melhores!!!</h2>
+                                <p>
+                                    Manter as contas em dia é sempre um problema? O MoneyZ avisa você: receba alertas de
+                                    contas a pagar e a receber!
+                                </p>
+                                <a href="" class="btn btn-primary">Veja mais</a>
+
+                            </div>
+                        </div>
+                    </section>
+                    <div class="row servicos mt-4 mb-4">
+                        <div class="col-md-4 text-light">
+                            <img src="img/fundoCSGO.jpg" alt="" class="img-fluid">
+                            <h4>Inteligencia Emocional:</h4>
+                            <p>
+                                Todo o jogo começa na sua mente. Para se tornar uma lenda no CS:GO, você precisa
+                                aprender a controlar seus
+                                pensamentos, estabelecendo uma mente focada e treinada para lidar com situações de
+                                grande pressão dentro da
+                                partida. Se você surtar de rage no meio de uma partida, você está fadado ao fracasso
+                            </p>
+                        </div>
+                        <div class="col-md-4 text-light">
+                            <img src="img/reflexo.jpg" alt="" class="img-fluid">
+                            <h4>Reflexos Apurados:</h4>
+                            <p>
+                                Do momento que você vê um adversário, até o momento que um dos dois são mortos, se
+                                passam milésimos de
+                                segundos. Por isso é necessário que a sua skill esteja altamente treinada, desse modo
+                                você terá sempre
+                                vantagem em caso de confronto direto. Se você não estiver com a skill bem treinada, será
+                                difícil segurar o
+                                posicionamento caso o time adversário tente avançar, fadando seu time ao fracasso.
+                            </p>
+                        </div>
+                        <div class="col-md-4 text-light">
+                            <img src="img/mapa.jpg" alt="" class="img-fluid">
+                            <h4>Táticas Matadoras:</h4>
+                            <p>
+                                Se você já jogou em um time sabe o quanto é importante o posicionamento dos jogadores
+                                dentro de um mapa.
+                                A
+                                estratégia por trás desse posicionamento é o que muda tudo. Se você tiver uma tática
+                                matadora, seu time será
+                                um
+                                dos destaques no cenário. Do contrário, caso seu time não tiver fundamentos táticos bem
+                                desenvolvidos, ele
+                                está
+                                fadado ao fracasso.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <footer class="row">
+                    <div class="col-md-4 d-flex justify-content-end">
+                        <a href="" class="btn btn-outline-light">
+                            <i class="fa fa-twitter fa-3x"></i>
+                        </a>
+                        <a href="" class="btn btn-outline-light ml-3">
+                            <i class="fa fa-youtube fa-3x"></i>
+                        </a>
+                        <a href="" class="btn btn-outline-light ml-3">
+                            <i class="fa fa-instagram fa-3x"></i>
+                        </a>
+                        <a href="" class="btn btn-outline-light ml-3">
+                            <i class="fa fa-facebook fa-3x"></i>
+                        </a>
+
+                    </div>
+                </footer>
+            </div>
+</body>
+
+</html>
